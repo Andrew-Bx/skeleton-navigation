e91532e734cd1f7b37a40666bb009213e81491be
andrew <38717153+Andrew-Bx@users.noreply.github.com>
2021-08-08 18:42:22 +0100

    use http2

--- skeleton-typescript/build/tasks/serve.js
+++ skeleton-typescript/build/tasks/serve.js
@@ -3,6 +3,17 @@ import { build } from './build';
 import { bundle } from './bundle';
 import { exportRelease } from './export-release';
 
+// hack!  trick http2 in to creating a secure server:
+// Trying to understand why this is needed...
+// In getHttpModule, when not specifying httpModule option, browser-sync explicitly chooses between the http and https modules:
+//   https://github.com/BrowserSync/browser-sync/blob/v2.27.5/packages/browser-sync/lib/server/utils.js#L120
+// both of which have a createServer method, which is called by getServer:
+//   https://github.com/BrowserSync/browser-sync/blob/v2.27.5/packages/browser-sync/lib/server/utils.js#L96
+// But when specifying httpModule='http2' we'll always use the http2 module, which has separate createServer and createSecureServer methods:
+//   https://github.com/nodejs/node/blob/master/lib/internal/http2/core.js#L3308
+const http2 = require('http2');
+http2.createServer = http2.createSecureServer;
+
 // this task utilizes the browsersync plugin
 // to create a dev server instance
 // at http://localhost:9000
@@ -27,6 +38,8 @@ function serveUsingBrowserSync(baseDir, done) {
     online: false,
     open: false,
     port: 9000,
+    https: true,
+    httpModule: 'http2',
     server: {
       baseDir: [baseDir],
       middleware: function(req, res, next) {
